99f504ff886c39909a8aa07ceddedb9456d8fc2f
Thanh0721 <thanhn2789@nycstudents.net>
2025-02-06 14:07:36 +0000

    set up

--- Book.java
+++ Book.java
@@ -1,16 +1,18 @@
 public class Book
-  {
-    private String title;
-    private double price;
+{
+  private String title;
+  private double price;
 
-public Book(String bookTitle, double bookPrice)
-    {
-    }
-public String getTitle()
-    {
-    }
-public String getBookInfo()
-    {
-      return title + "-" + price;
-    }
+  public Book(String bookTitle, double bookPrice)
+  {
   }
+
+  public String getTitle()
+  {
+  }
+
+  public String getBookInfo()
+  {
+    return title + "-" + price;
+  }
+}
