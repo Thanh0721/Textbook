7f345cea14a53c710d73c2aafa38f6d4cc915d68
Thanh0721 <thanhn2789@nycstudents.net>
2025-02-10 14:28:01 +0000

    update

--- Textbook.java
+++ Textbook.java
@@ -1,4 +1,10 @@
-public class Textbook
+public class Textbook extends Book
 {
-    
+    private int edition;
+}
+
+public Textbook(String title, double price, int edition)
+{
+    super(titile, price);
+    this.edition = edition;
 }
