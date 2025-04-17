2c96365ab8fa05906b0f6ac7354b9504f7869b50
Thanh0721 <thanhn2789@nycstudents.net>
2025-04-17 18:35:07 +0000

    update

--- Book.java
+++ Book.java
@@ -1,17 +1,19 @@
-public class Book
-{
+public class Book {
   private String title;
   private double price;
 
-  public Book(String bookTitle, double bookPrice)
+  public Book(String bookTitle, double bookPrice) 
   {
+    title = bookTitle;
+    price = bookPrice;
   }
 
-  public String getTitle()
+  public String getTitle() 
   {
+    return title;
   }
 
-  public String getBookInfo()
+  public String getBookInfo() 
   {
     return title + "-" + price;
   }
--- Textbook.java
+++ Textbook.java
@@ -1,10 +1,30 @@
-public class Textbook extends Book
-{
+public class Textbook extends Book {
     private int edition;
-}
 
-public Textbook(String title, double price, int edition)
-{
-    super(titile, price);
-    this.edition = edition;
+    public Textbook(String title, double price, int edition) 
+    {
+        super(title, price);
+        this.edition = edition;
+    }
+
+    public int getEdition() 
+    {
+        return edition;
+    }
+
+    @Override
+    public String getBookInfo() 
+    {
+        return super.getBookInfo() + "-" + edition;
+    }
+
+    public boolean canSubstituteFor(Textbook other) 
+    {
+        if (this.getTitle().equals(other.getTitle())) 
+        {
+            return this.edition >= other.edition;
+        }
+        
+        return false;
+    }
 }
